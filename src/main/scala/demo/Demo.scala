46f67b636b2cef17165caa86332b964636e08038
Vouillamoz_Hugo <hugovouillamoz@hotmail.ch>
2022-11-23 20:57:09 +0100

    menu switch 1-10

--- src/main/scala/demo/Demo.scala
+++ src/main/scala/demo/Demo.scala
@@ -4,6 +4,7 @@ import com.github.tototoshi.csv.*
 
 import java.io.File
 import scala.io.Source
+import scala.io.StdIn.readLine
 
 object Demo {
   def main(args: Array[String]): Unit = {
@@ -35,30 +36,32 @@ object Demo {
     //Retrieve data from csv file
     val data = Source.fromFile(csvFilename).getLines.toList
 
-    val allSong=new java.util.ArrayList[Song]
+    val allSong = new java.util.ArrayList[Song]
 
     //Print all lines of data
     for (i <- data) {
       // println(i)
       val cols = i.split(";")
 
-      if(cols.length > 8) {
+      if (cols.length > 8) {
         //      val mySong = new Song(cols(0),cols(1),cols(2),cols(3),cols(4),cols(5),cols(6),cols(7).toInt,cols(8).toInt)
-        val mySong = new Song(cols(0), cols(1), cols(2), cols(3), cols(4), cols(5), cols(6), cols(7),cols(8))
+        val mySong = new Song(cols(0), cols(1), cols(2), cols(3), cols(4), cols(5), cols(6), cols(7), cols(8))
         allSong.add(mySong)
-      }else {
-        val mySong = new Song(cols(0), cols(1), cols(2), cols(3), cols(4), cols(5), cols(6), "","")
+      } else {
+        val mySong = new Song(cols(0), cols(1), cols(2), cols(3), cols(4), cols(5), cols(6), "", "")
         allSong.add(mySong)
       }
-      println(allSong.size())
-
     }
 
-    //    allSong.forEach(song =>{
-    //      print(song.title + " | ")
-    //      println(song.songArtist.name + " | ")
-    //
-    //    })
+    while(true) {
+      val userChoice = readLine("Hurry up choose the query you want little bitch \n 1. Fuck you\n2. Fuck you\n3. Fuck you\n4. Fuck you\n5. Fuck you\n6. Fuck you\n7. Fuck you\n8. Fuck you\n9. Fuck you\n10. Fuck you\n");
+      if (userChoice.toInt > 0 && userChoice.toInt < 10) {
+        println("Your bad choice was " + userChoice.toString)
+      } else {
+        println("Select a number between 1 and 10 stupid wanier")
+      }
+    }
+
 
     val titles = data.filter({ i => i.charAt(0) == 'L' })
     //    for (i <- titles) {
@@ -71,6 +74,9 @@ object Demo {
     //      reader.foreach(fields => println(fields))
     //      reader.foreach(fields => println(fields))
     //      reader.close()
+
+    //TODO
+    // 1 get all song for a user
 
 
   }
